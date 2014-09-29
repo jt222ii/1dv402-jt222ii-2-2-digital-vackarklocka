843eb456ed8d11cf41b04bc2d73fc5dc5b353414
jt222ii <jt222ii@student.lnu.se>
2014-09-29 16:02:34 +0200

    Probably done!

--- 1dv402-jt222ii-2-1-gissa-det-hemliga-talet/1DV402.S2.L1A/1DV402.S2.L1A/SecretNumber.cs
+++ 1dv402-jt222ii-2-1-gissa-det-hemliga-talet/1DV402.S2.L1A/1DV402.S2.L1A/SecretNumber.cs
@@ -8,10 +8,10 @@ namespace _1DV402.S2.L1A
 {
     class SecretNumber
     {
-        private int _count;
-        private int _number;
+        private int _count;  // antal gissningar gjordas
+        private int _number; // hemliga numret förvaras här
         public const int MaxNumberOfGuesses = 7;
-        public SecretNumber()
+        public SecretNumber() // startar initialize
         {
             Initialize();
         }
@@ -19,40 +19,40 @@ namespace _1DV402.S2.L1A
         {
             _count = 0;
             Random random = new Random();
-            _number = random.Next(1, 101);
+            _number = random.Next(1, 101); //_number får ett slumpvalt tal från ett till 100
         }
         public bool MakeGuess(int number)
         {
-            if (_count >= MaxNumberOfGuesses)
+            if (_count >= MaxNumberOfGuesses)   //om _count(antal försök) överstiger eller är 7 så kastas ett undantag. Annars _count + 1 och man har ett till försök
             {
-                Console.WriteLine("Du har slut på gissningar - du är sämst");
                 throw new ApplicationException();
             }
             _count++;
-            if (number < 1 || number > 100)
+            if (number == _number)              // Jämför numret användaren skriver in med det hemliga talet
             {
-                throw new ArgumentOutOfRangeException();               
-            }
-
-            if (number == _number)
-            {
-
-                Console.WriteLine("{0} var rätt!", number);
+                Console.WriteLine("GRATTIS, {0} var rätt! Du klarade det på {1} försök!", number, _count);
                 return true;
             }
-            if (number > _number)
+            if (_count == MaxNumberOfGuesses)   // om _count är lika stort som MaxNumberOfGuesses så har man förbrukat alla sina försök
+            {
+                Console.WriteLine("Du har förbrukat alla dina {0} försök. Det rätta svaret var: {1}", MaxNumberOfGuesses, _number);
+                return false;
+            }
+            if (number < 1 || number > 100)    //om man skiver in ett nummer mindre än 1 eller större än 100 kastas ett undantag
+            {
+                throw new ArgumentOutOfRangeException();
+            }
+            if (number > _number) // om numret man skrev in är större än det hemliga talet säger konsollen att det var för högt och även hur många försök kvar man har
             {
                 Console.WriteLine("{0} är för högt. Var god försök igen. Du har {1} gissningar kvar", number, (MaxNumberOfGuesses - _count));
                 return false;
             }
-            if (number < _number)
+            if (number < _number) // samma som ovan fast för nummer mindre än det hemliga numret
             {
                 Console.WriteLine("{0} är för lågt. Var god försök igen. Du har {1} gissningar kvar", number, (MaxNumberOfGuesses - _count));
                 return false;
             }
-            else
-                System.Console.WriteLine("Det hemliga talet är {0}.", _number);
-                return false;   
+            return false;
         }
     }
 }
