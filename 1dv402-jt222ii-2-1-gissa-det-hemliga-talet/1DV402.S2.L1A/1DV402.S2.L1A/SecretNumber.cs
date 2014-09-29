73e791ab059c2b7042ba81f0adb3be2655670158
jt222ii <jt222ii@student.lnu.se>
2014-09-29 15:05:50 +0200

    test

---
+++ 1dv402-jt222ii-2-1-gissa-det-hemliga-talet/1DV402.S2.L1A/1DV402.S2.L1A/SecretNumber.cs
@@ -0,0 +1,64 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _1DV402.S2.L1A
+{
+    class SecretNumber
+    {
+        
+        private int _count;
+        private int _number;
+        public const int MaxNumberOfGuesses = 7;
+        public SecretNumber()
+        {
+            Initialize();
+        }
+        public void Initialize() // sätter _count till 0 och ger _number ett random nummer mellan 1-100
+        {
+            _count = 0;
+            Random random = new Random();
+            _number = random.Next(1, 101);
+        }
+        public bool MakeGuess(int number)
+        {
+
+            if (_count >= MaxNumberOfGuesses)
+            {
+                Console.WriteLine("Du har slut på gissningar - du är sämst");
+                throw new ApplicationException();
+            }
+
+            _count++;
+
+
+            if (number < 1 || number > 100)
+            {
+                throw new ArgumentOutOfRangeException();               
+            }
+
+            if (number == _number)
+            {
+
+                Console.WriteLine("{0} var rätt!", number);
+                return true;
+            }
+            if (number > _number)
+            {
+                Console.WriteLine("{0} är för högt. Var god försök igen. Du har {1} gissningar kvar", number, (MaxNumberOfGuesses - _count));
+                return false;
+            }
+            if (number < _number)
+            {
+                Console.WriteLine("{0} är för lågt. Var god försök igen. Du har {1} gissningar kvar", number, (MaxNumberOfGuesses - _count));
+                return false;
+            }
+            else
+                System.Console.WriteLine("Det hemliga talet är {0}.", _number);
+                return false;
+   
+        }
+    }
+}
