b8246ced7512f8a4b2d2f46131be8c13c66b682c
jt222ii <jt222ii@student.lnu.se>
2014-10-06 19:53:18 +0200

    I think im done. Finished test 1-7 and added comment. Will look for errors.

--- Digital_Vackarklocka/Digital_Vackarklocka/AlarmClock.cs
+++ Digital_Vackarklocka/Digital_Vackarklocka/AlarmClock.cs
@@ -13,67 +13,73 @@ namespace Digital_Vackarklocka
         private int _hour;
         private int _minute;
 
-        public int AlarmHour
+        //Get är för att läsa ett värde. Set är för att ändra ett värde. 
+        //EX) När Program.cs behöver värdet på _alarmHour så används get. 
+        //    Men när program.cs skickar in värden så används set.
+        public int AlarmHour  // Alarmets timmar. Se ovan för förklaring av get set
         {
             get { return _alarmHour; }
             set
             {
                 if (value < 0 || value > 23)
-                { throw new ArgumentException("Talet är inte inom det rätta intervallet 0-23"); }
+                { throw new ArgumentException("alarmets timmar är inte inom det rätta intervallet 0-23"); }
                 _alarmHour = value;
             }
         }
-        public int AlarmMinute
+        public int AlarmMinute  // Alarmets minuter. Se ovan för förklaring av get set
         {
-            get { return _alarmMinute; }
+            get { return _alarmMinute; } 
             set
             {
                 if (value < 0 || value > 59)
-                { throw new ArgumentException("Talet är inte inom det rätta intervallet 0-59"); }
+                { throw new ArgumentException("alarmets minuter är inte inom det rätta intervallet 0-59"); }
                 _alarmMinute = value;
             }
         }
-        public int Hour
+        public int Hour // Klockans timmar. Se ovan för förklaring av get set
         {
             get { return _hour; }
             set
             {
                 if (value < 0 || value > 23)
                 { 
-                    throw new ArgumentException("Talet är inte inom det rätta intervallet 0-23"); 
+                    throw new ArgumentException("klockans timmar är inte inom det rätta intervallet 0-23"); 
                 }
                 _hour = value; 
             }
         }
-        public int Minute
+        public int Minute   // Klockans minuter. Se ovan för förklaring av get set
         {
             get { return _minute; }
             set
             {
                 if (value < 0 || value > 59)
-                { throw new ArgumentException("Talet är inte inom det rätta intervallet 0-59"); }
+                { throw new ArgumentException("klockans minuter är inte inom det rätta intervallet 0-59"); }
                 _minute = value;
             }
         }
 
-        //metoder här
-        public AlarmClock(): this(0,0)
+        
+        //konstruktorerna ser till att objekten blir korrekt initierade
+        public AlarmClock(): this(0,0) //anropar konstruktorn med 2 parameterfält 
         {
             // "Ingen tilldelning får ske i konstruktorns kropp, som måste vara tom."
             //"Denna konstruktor måste därför anropa den konstruktor i klassen som har två parametrar." - uppgiften
         }
-        public AlarmClock(int hour, int minute):this (hour,minute,0,0)
-        {
-
+        public AlarmClock(int hour, int minute):this (hour,minute,0,0)//anropar konstruktorn med 4 parameterfält                                                                       
+        {                                                           
+            //Ingen tilldelning får ske i konstruktorns kropp, som måste vara tom. Denna konstruktor måste därför anropa den 
+            //konstruktor i klassen som har fyra parametrar.
         }
-        public AlarmClock(int hour, int minute, int alarmHour, int alarmMinute)
+        public AlarmClock(int hour, int minute, int alarmHour, int alarmMinute) //tilldelar värden till objekten och skickar tillbaka dem via de andra två konstruktorerna
         {
             Hour = hour;
             Minute = minute;
             AlarmHour = alarmHour;
             AlarmMinute = alarmMinute;
         }
-        public bool TickTock()
+        public bool TickTock() //varje gång ticktock anropas ökas minuten med 1. Om antal minuter överstiger 59 sätts minuter till 0 och our får +1. Hour testas då om det är mer än 23. 
+                               // stämmer det sätts timtalet till 0. Sedan testas det om timmen och minuten stämmer med alarmets timme och minut. 
         {
             _minute++;
             if (_minute>59)
@@ -85,9 +91,9 @@ namespace Digital_Vackarklocka
             else
                 return false;
         }
-        public string ToString()
+        public string ToString()  //skriver ut klockslaget och inställd alarmtid
         {
-            StringBuilder time = new StringBuilder();
+            StringBuilder time = new StringBuilder();       //stringbuilder kan lägga till text och istället för att man skickar tillbaka ett nytt objekt kan man returnera samma objekt med modifierade värden
             time.AppendFormat("{0,4}:", _hour);
             if(_minute<10)
             { 
@@ -106,8 +112,9 @@ namespace Digital_Vackarklocka
             {
                 time.AppendFormat("{0}>", _alarmMinute);
             }
-
-            return time.ToString();
+                                                             //AppendFormat fungerar som så att använda string format fast för append istället. 
+                                                            //Ex) "time.AppendFormat("0{0}", _minute);  " hade skrivits "time.Append("0"); time.Append(_minute);" för att få samma resultat som utan Format
+            return time.ToString();                        //returnerar StringBuilder objektet som har fått sitt värde av time.AppendFormat
         }
     }
 }
--- Digital_Vackarklocka/Digital_Vackarklocka/Program.cs
+++ Digital_Vackarklocka/Digital_Vackarklocka/Program.cs
@@ -8,20 +8,67 @@ namespace Digital_Vackarklocka
 {
     class Program
     {
+        private static string HorizontalLine = "\n|=================================================================|";
         static void Main(string[] args)
         {
-            AlarmClock test1 = new AlarmClock();
+
+            AlarmClock test1 = new AlarmClock();                       //test 1 kollar standardkonstruktorn så att det blir 0:00
+            ViewTestHeader("\n Test 1 \nTest av standardkonstruktorn.");
             Console.WriteLine(test1.ToString());
 
-            AlarmClock test2 = new AlarmClock(9, 42);
+            AlarmClock test2 = new AlarmClock(9, 42);                   //test två kollar konstruktorn så att det blir 9:42 - eftersom vi skickat med 9 och 42
+            ViewTestHeader("\n Test 2 \nTest av konstruktorn med två parametrar (9, 42)");
             Console.WriteLine(test2.ToString());
 
-            AlarmClock test3 = new AlarmClock(13, 24, 7, 35);
+            AlarmClock test3 = new AlarmClock(13, 24, 7, 35);           //test 3 kollar så att konstruktorn även kan ta emot värden för alarmet (med de värden vi skickar med ska det bli 13:24 <7:35>)
+            ViewTestHeader("\n Test 3 \nTest av konstruktorn med fyra parametrar, (13, 24, 7, 35)");
             Console.WriteLine(test3.ToString());
 
-            AlarmClock test4 = new AlarmClock(23, 58, 7, 35);
-            ViewTestHeader("\n Test 4 \nStäller AlarmClock-objektet till 23:58 och låter den gå 13 'minuter' ");
-            Run(test4, 13);
+            AlarmClock test4 = new AlarmClock(23, 58, 7, 35);           //test 4 skickar med värden och sätter klockan till 23:58 och alarmet till 7:35. 
+            ViewTestHeader("\n Test 4 \nStäller befintligt AlarmClock-objekt till 23:58 och låter den gå 13 'minuter' ");
+            Run(test4, 13);                                             //Kör sedan Run-metoden som loopar igenom så många gånger vi skickar med att den ska göra. I detta fallet 13 ggr                                                                                         
+
+            AlarmClock test5 = new AlarmClock(6, 12, 6, 15);            //test 5 gör som 4 fast låter klockan gå endast 6 minuter och förbi en tid då "larmet" är ställt - programmet ska då skriva ut alarmet.
+            ViewTestHeader("\n Test 5 \nStäller befintligt AlarmClock-objekt till 6:12 och låter den gå 6 'minuter' ");
+            Run(test5, 6);
+
+            AlarmClock test6 = new AlarmClock(); //testar klockans timmar och minuter och alarmets timmar och minuter. Jag tilldelar värden som kommer ge fel för att se att så att ArgumentException kastas
+            ViewTestHeader("\n Test 6 \nTestar egenskaperna så att undantag kastas då tid och alarmtid \ntilldelas felaktiga värden.");
+            try
+            { test6.Hour = 24; }                   
+            catch (ArgumentException wrong)
+            { ViewErrorMessage(wrong.Message); }
+            try
+            { test6.Minute = 60; }
+            catch (ArgumentException wrong)
+            { ViewErrorMessage(wrong.Message); }
+            try
+            { test6.AlarmHour = 25; }
+            catch (ArgumentException wrong)
+            { ViewErrorMessage(wrong.Message); }
+            try
+            { test6.AlarmMinute = 60; }
+            catch (ArgumentException wrong)
+            { ViewErrorMessage(wrong.Message); }
+
+            
+            ViewTestHeader("\n Test 7 \nTestar konstruktorer så att undantag kastas då tid och alarmtid \ntilldelas felaktiga värden."); // samma test som ovan fast genom konstruktorer som sedan skickar värdena till egenskaperna som på sin tur kommer kasta undantag.
+            try
+            { AlarmClock test7 = new AlarmClock(24, 0); }
+            catch (ArgumentException wrong)
+            { ViewErrorMessage(wrong.Message); }
+            try
+            { AlarmClock test7 = new AlarmClock(0, 60); }
+            catch (ArgumentException wrong)
+            { ViewErrorMessage(wrong.Message); }
+            try
+            { AlarmClock test7 = new AlarmClock(0, 0, 24, 0); }
+            catch (ArgumentException wrong)
+            { ViewErrorMessage(wrong.Message); }
+            try
+            { AlarmClock test7 = new AlarmClock(0, 0, 0, 60); }
+            catch (ArgumentException wrong)
+            { ViewErrorMessage(wrong.Message); }
         }
         private static void Run(AlarmClock test, int minutes)
         {
@@ -32,23 +79,26 @@ namespace Digital_Vackarklocka
                     Console.BackgroundColor = ConsoleColor.DarkGreen;
                     Console.Write("♫");
                     Console.Write(test.ToString());
-                    Console.WriteLine("beeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeep");
+                    Console.WriteLine("Beeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeep!");
                 }
                 else
                 {
                     Console.Write(" ");
-                    Console.WriteLine(test.ToString()); 
+                    Console.WriteLine(test.ToString());
                 }
                 Console.ResetColor();
             }
         }
-        private static void ViewErrorMessage(string message)
-        {
-
-        }
-        private static void ViewTestHeader(string header)
+        private static void ViewErrorMessage(string message)        //skriver ut error-meddelandet som från början kom från AlarmClock.cs
         {
             Console.BackgroundColor = ConsoleColor.DarkRed;
+            Console.WriteLine(message);
+            Console.ResetColor();
+        }
+        private static void ViewTestHeader(string header)  //Skriver ut vilket test som körs samt linjen som avskiljer dem
+        {
+            Console.WriteLine(HorizontalLine);
+            Console.ForegroundColor = ConsoleColor.Green;
             Console.WriteLine(header);
             Console.ResetColor();
         }
