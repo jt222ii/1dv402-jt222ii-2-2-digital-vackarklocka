062a6a09dfb76745799fffc6bf534feb9a5d55bc
jt222ii <jt222ii@student.lnu.se>
2014-10-06 16:14:34 +0200

    Finished AlarmClock.cs started test

---
+++ Digital_Vackarklocka/Digital_Vackarklocka/AlarmClock.cs
@@ -0,0 +1,113 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Digital_Vackarklocka
+{
+    class AlarmClock
+    {
+        private int _alarmHour;
+        private int _alarmMinute;
+        private int _hour;
+        private int _minute;
+
+        public int AlarmHour
+        {
+            get { return _alarmHour; }
+            set
+            {
+                if (value < 0 || value > 23)
+                { throw new ArgumentException("Talet är inte inom det rätta intervallet 0-23"); }
+                _alarmHour = value;
+            }
+        }
+        public int AlarmMinute
+        {
+            get { return _alarmMinute; }
+            set
+            {
+                if (value < 0 || value > 59)
+                { throw new ArgumentException("Talet är inte inom det rätta intervallet 0-59"); }
+                _alarmMinute = value;
+            }
+        }
+        public int Hour
+        {
+            get { return _hour; }
+            set
+            {
+                if (value < 0 || value > 23)
+                { 
+                    throw new ArgumentException("Talet är inte inom det rätta intervallet 0-23"); 
+                }
+                _hour = value; 
+            }
+        }
+        public int Minute
+        {
+            get { return _minute; }
+            set
+            {
+                if (value < 0 || value > 59)
+                { throw new ArgumentException("Talet är inte inom det rätta intervallet 0-59"); }
+                _minute = value;
+            }
+        }
+
+        //metoder här
+        public AlarmClock(): this(0,0)
+        {
+            // "Ingen tilldelning får ske i konstruktorns kropp, som måste vara tom."
+            //"Denna konstruktor måste därför anropa den konstruktor i klassen som har två parametrar." - uppgiften
+        }
+        public AlarmClock(int hour, int minute):this (hour,minute,0,0)
+        {
+
+        }
+        public AlarmClock(int hour, int minute, int alarmHour, int alarmMinute)
+        {
+            Hour = hour;
+            Minute = minute;
+            AlarmHour = alarmHour;
+            AlarmMinute = alarmMinute;
+        }
+        public bool TickTock()
+        {
+            _minute++;
+            if (_minute>59)
+            { _hour++; _minute = 0; }
+            if(_hour>23)
+            { Hour = 0; }
+            if (_hour == _alarmHour && _minute == _alarmMinute)
+            { return true; }
+            else
+                return false;
+        }
+        public string ToString()
+        {
+            StringBuilder time = new StringBuilder();
+            time.AppendFormat("{0,4}:", _hour);
+            if(_minute<10)
+            { 
+                time.AppendFormat("0{0}", _minute); 
+            }
+            else
+            { 
+                time.AppendFormat("{0}", _minute);
+            }
+            time.AppendFormat("<{0}:", _alarmHour);
+            if(_alarmMinute<10)
+            { 
+                time.AppendFormat("0{0}>", _alarmMinute);
+            }
+            else
+            {
+                time.AppendFormat("{0}>", _alarmMinute);
+            }
+
+            return time.ToString();
+        }
+    }
+}
---
+++ Digital_Vackarklocka/Digital_Vackarklocka/Program.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Digital_Vackarklocka
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            AlarmClock test1 = new AlarmClock();
+            Console.WriteLine(test1.ToString());
+
+            AlarmClock test2 = new AlarmClock(9, 42);
+            Console.WriteLine(test2.ToString());
+
+            AlarmClock test3 = new AlarmClock(13, 24, 7, 35);
+            Console.WriteLine(test3.ToString());
+
+            AlarmClock test4 = new AlarmClock(23, 58, 7, 35);
+            ViewTestHeader("\n Test 4 \nStäller AlarmClock-objektet till 23:58 och låter den gå 13 'minuter' ");
+            Run(test4, 13);
+        }
+        private static void Run(AlarmClock test, int minutes)
+        {
+            for (int i = 0; i < minutes; i++)
+            {
+                if (test.TickTock())
+                {
+                    Console.BackgroundColor = ConsoleColor.DarkGreen;
+                    Console.Write("♫");
+                    Console.Write(test.ToString());
+                    Console.WriteLine("beeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeep");
+                }
+                else
+                {
+                    Console.Write(" ");
+                    Console.WriteLine(test.ToString()); 
+                }
+                Console.ResetColor();
+            }
+        }
+        private static void ViewErrorMessage(string message)
+        {
+
+        }
+        private static void ViewTestHeader(string header)
+        {
+            Console.BackgroundColor = ConsoleColor.DarkRed;
+            Console.WriteLine(header);
+            Console.ResetColor();
+        }
+
+    }
+}
